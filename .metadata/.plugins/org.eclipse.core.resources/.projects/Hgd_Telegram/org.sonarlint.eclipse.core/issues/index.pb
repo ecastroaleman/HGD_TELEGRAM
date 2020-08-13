3ae2b7db6cb4bf29b6dfbcdcc4d77c3a883b11d4
ECANJOE <emilio.castro.aleman@ericsson.com>
2020-08-13 08:27:28 -0600

    Inicial

---
+++ .metadata/.plugins/org.eclipse.core.resources/.projects/Hgd_Telegram/org.sonarlint.eclipse.core/issues/index.pb
@@ -0,0 +1,15 @@
+
+[
++src/main/java/com/ericsson/hgd/Tickets.java,0\3\039d70f1bedf7d9174b69ed586067e26c7974abb
+@
+log4j.properties,7\3\7330e4252652b61093498eabc26c3cadee5d89b7
+[
++src/main/java/com/ericsson/hgd/Inicial.java,9\f\9f681bc5ffc32f99eea9d6d051264d267d8615e4
+[
++src/main/java/com/ericsson/hgd/SendMsg.java,2\2\224f845c80a7a7aa4a3f192c7a18605509d80330
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+W
+'src/main/java/com/ericsson/hgd/App.java,1\b\1b662cc1ea6808ff0e7692bb72e3005313402559
+i
+9src/main/java/com/ericsson/hgd/ApplicationProperties.java,b\7\b775b745dad42ef67f648ca91c1b569f40a5127c
---
+++ Hgd_Telegram/src/main/java/com/ericsson/hgd/Tickets.java
@@ -0,0 +1,60 @@
+package com.ericsson.hgd;
+
+public class Tickets {
+	public String detectionOn;
+	public String getDetectionOn() {
+		return detectionOn;
+	}
+
+	public void setDetectionOn(String det) {
+		this.detectionOn = det;
+	}
+	public String key;
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public String getDateCreated() {
+		return dateCreated;
+	}
+
+	public void setDateCreated(String dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
+	public String getFixVersion() {
+		return fixVersion;
+	}
+
+	public void setFixVersion(String fixVersion) {
+		this.fixVersion = fixVersion;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String dateCreated;
+	public String fixVersion;
+	public String createdBy;
+	public String status;
+
+	
+
+}
